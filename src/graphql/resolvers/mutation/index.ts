4b2c359ac92ee0434156e46176780328539782c4
ganhuyagdamdinnym <damdinnymg@gmail.com>
2024-07-23 22:25:53 +0800

    space test

--- src/graphql/resolvers/mutation/index.ts
+++ src/graphql/resolvers/mutation/index.ts
@@ -1,5 +1,7 @@
 export * from "./user/createUser";
 export * from "./user/loginUser";
+
 export * from "./product/createTravel";
+
 export * from "./product/deleteProduct";
 export * from "./user/verificationCode";
